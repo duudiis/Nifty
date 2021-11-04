3050169aa869f2fe9ac3a81efd823e8d82cca9bb
Dudisss <dudu.obladen@gmail.com>
2021-11-04 11:57:28 -0300

    Update 247.js

--- src/commands/configuration/247.js
+++ src/commands/configuration/247.js
@@ -10,7 +10,7 @@ module.exports = class Forever extends Commands {
         this.client = client;
 
         this.name = "247";
-        this.aliases = ["24/7", "forever", "infinity"];
+        this.aliases = ["24/7", "forever", "infinity", "nonstop"];
 
         this.description = "Toggles 24/7 mode";
         this.category = "music";
